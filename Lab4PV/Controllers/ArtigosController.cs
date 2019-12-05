2544a6e94e69930b56bc095098489e2d7a6186fd
weedfam <58572351+weedfam@users.noreply.github.com>
2019-12-05 21:54:34 +0000

    Add files via upload

---
+++ Lab4PV/Controllers/ArtigosController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ESTaPapelaria.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESTaPapelaria.Controllers
+{
+    public class ArtigosController : Controller
+    {
+        private List<Artigo> artigos = Program.BDArtigos;
+        public IActionResult Listar()
+        {
+            return View(artigos);
+        }
+
+        public IActionResult Promocoes()
+        {
+            return View(artigos.Where(a => a.EmPromocao));
+        }
+
+        [HttpPost]
+        public IActionResult Listar(string filtro)
+        {
+            var artigosFiltrados = from a in artigos select a;
+
+            if (!String.IsNullOrEmpty(filtro))
+            {
+                artigosFiltrados = artigosFiltrados.Where(a => a.Nome.Contains(filtro));
+            }
+
+            return View(artigosFiltrados.ToList());
+        }
+
+        public IActionResult Editar(int? id)
+        {
+            Artigo artigo = artigos.Find(a => a.Id == id);
+
+            return View(artigo);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(int id, Artigo artigo)
+        {
+            Artigo ArtigoNaBD = artigos.Find(a => a.Id == id);
+
+            ArtigoNaBD.Desconto = artigo.Desconto;
+            ArtigoNaBD.Descricao = artigo.Descricao;
+            ArtigoNaBD.EmPromocao = artigo.EmPromocao;
+            ArtigoNaBD.Nome = artigo.Nome;
+            ArtigoNaBD.Preco = artigo.Preco;
+
+            return View(ArtigoNaBD);
+        }
+
+    }
+}
---
+++ Lab4PV/Models/Artigo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESTaPapelaria.Models
+{
+    public class Artigo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public double Desconto { get; set; }
+        public bool EmPromocao { get; set; }
+    }
+}
